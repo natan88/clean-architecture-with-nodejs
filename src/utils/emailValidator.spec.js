2b2dc32a7cf4b3c376f8afbb1e9e3f8d82f3c0cd
Natanael Freire <bsbinfor@gmail.com>
2021-11-25 22:09:20 -0300

    ensure EmailValidator calls validator with correct email

--- src/utils/emailValidator.spec.js
+++ src/utils/emailValidator.spec.js
@@ -15,10 +15,17 @@ describe('Email Validator', () => {
     const isEmailValid = sut.isValid('valid_email@gmail.com')
     expect(isEmailValid).toBe(true)
   })
+
   test('Should return false if validator returns false', () => {
     validator.isEmailValid = false
     const sut = makeSut()
-    const isEmailValid = sut.isValid('invalid_email@gmail .com')
+    const isEmailValid = sut.isValid('invalid_email@gmail.com')
     expect(isEmailValid).toBe(false)
+  })
+
+  test('Should call validator with correct email', () => {
+    const sut = makeSut()
+    sut.isValid('any_email@gmail.com')
+    expect(validator.email).toBe('any_email@gmail.com')
   })
 })
