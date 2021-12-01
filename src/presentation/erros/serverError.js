eb2c91e1a7c6664b9636fa7c86df38b2dec48d8b
Natanael Freire <bsbinfor@gmail.com>
2021-12-01 17:33:02 -0300

    revert changes

--- src/presentation/erros/serverError.js
+++ src/presentation/erros/serverError.js
@@ -1,6 +1,6 @@
 module.exports = class ServerError extends Error {
   constructor (paramName) {
-    super('Internal error123')
+    super('Internal error')
     this.name = this.constructor.name
   }
 }
