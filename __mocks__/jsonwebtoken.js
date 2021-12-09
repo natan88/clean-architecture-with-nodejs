765873413f5a8f35b51b93d2f5f46e94991df287
Natanael Freire <bsbinfor@gmail.com>
2021-12-09 20:49:50 -0300

    ensure call JWT with correct values

--- __mocks__/jsonwebtoken.js
+++ __mocks__/jsonwebtoken.js
@@ -1,7 +1,11 @@
 module.exports = {
   token: 'any_token',
+  key: '',
+  secret: '',
 
   sign (key, secret) {
+    this.key = key
+    this.secret = secret
     return this.token
   }
 }
--- src/utils/helpers/tokenGenerator.spec.js
+++ src/utils/helpers/tokenGenerator.spec.js
@@ -1,21 +1,36 @@
 const jwt = require('jsonwebtoken')
 class TokenGenerator {
-  async generate (key) {
-    return jwt.sign(key, 'secret')
+  constructor (secret) {
+    this.secret = secret
   }
+
+  async generate (key) {
+    return jwt.sign(key, this.secret)
+  }
+}
+
+const makeSut = () => {
+  return new TokenGenerator('secret')
 }
 
 describe('Token Generator', () => {
   test('Should return null if JWT returns null', async () => {
-    const sut = new TokenGenerator()
+    const sut = makeSut()
     jwt.token = null
-    const token = await sut.generate('any_id')
+    const token = await sut.generate('any_key')
     expect(token).toBeNull()
   })
 
   test('Should return a token if JWT returns token', async () => {
-    const sut = new TokenGenerator()
-    const token = await sut.generate('any_id')
+    const sut = makeSut()
+    const token = await sut.generate('any_key')
     expect(token).toBe(jwt.token)
+  })
+
+  test('Should call JWT with correct values', async () => {
+    const sut = makeSut()
+    await sut.generate('any_key')
+    expect(jwt.key).toBe('any_key')
+    expect(jwt.secret).toBe(sut.secret)
   })
 })
